300e65bbf30608a149e3f286b9cfd6e1e9bdfa4c
cxyluxixi <42094356+cxyluxixi@users.noreply.github.com>
2019-01-11 16:57:21 +0800

    Update login_weibo.py

--- python-practice/login_weibo.py
+++ python-practice/login_weibo.py
@@ -12,6 +12,7 @@ import base64
 import re
 import json
 import hashlib
+import random
 
 cj = http.cookiejar.LWPCookieJar()
 cookie_support = urllib.request.HTTPCookieProcessor(cj)
@@ -53,6 +54,7 @@ Accept-Encoding: gzip, deflate, br
 Accept-Language: zh-CN,zh;q=0.9
 
 '''
+# 将headers, postdata
 def str2obj(s,s1='\n',s2=': '):
    li = s.split(s1)
    res = {}
@@ -64,20 +66,21 @@ def str2obj(s,s1='\n',s2=': '):
 
 
 def get_servertime():
+   # 这里的urltime 需要去自己的浏览器里搜索，servertime在哪个文件里面，找到里面请求的url，复制过来
    urltime = 'https://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=MTM4NzExMzQyMDk%3D&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.19)&_=1547170677962'
    # 返回出来的是一个Response对象，无法直接获取，text后，可以通过正则匹配到
    # 大概长这样子的：sinaSSOController.preloginCallBack({"retcode":0,"servertime":1545606770, ...})
    data = requests.request('GET', urltime).text
-   p = re.compile('((.*))')
+   p = re.compile('(({.*}))')
    try:
-       json_data = p.search(data).group(1)
-       data = json.loads(json_data)
-       servertime = str(data['servertime'])
-       nonce = data['nonce']
-       return servertime, nonce
+      json_data = p.search(data).group(1)
+      d = json.loads(json_data)
+      servertime = str(d['servertime'])
+      nonce = d['nonce']
+      return servertime, nonce
    except:
-       print('获取 severtime 失败!')
-       return None
+      print('获取 severtime 失败!')
+      return None
 
 
 def get_pwd(pwd, servertime, nonce):
@@ -99,27 +102,34 @@ def get_user(username):
    username = base64.encodebytes(_username.encode())[:-1]
    return username
 
+# 获取各种参数，添加进去，比如用户名，密码，server time，nonce，加入到postdata里
+
 username = input('username')
 pwd=input('password')
 stime,nonce = get_servertime()
 pword = get_pwd(pwd,stime,nonce)
-
-
-headers = str2obj(headers)
 postdata['servertime'] = stime
 postdata['nonce']=nonce
 postdata['sp']= pword
 postdata['su']= str(get_user(username))
 
+# 规范headers的格式，成为字典
+headers = str2obj(headers)
 
-url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
-loginData = requests.post(url,params=postdata,headers=headers)
-ret = loginData.text
-p = re.compile(r'location\.replace\(\"(.*?)\"\)')
-try:
-   login_url = p.search(ret).group(1)
-   r = requests.request('GET',login_url)
-   r.raise_for_status()
-   print("success")
-except:
-   print('failure')
+
+if __name__ == "__main__":
+   url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
+   loginData = requests.post(url,params=postdata,headers=headers)
+   ret = loginData.text
+
+   # 正则，在上述返回的ret中，获取目标login_url
+   p = re.compile(r'location\.replace\(\"(.*?)\"\)')
+   try:
+      # 获取目标login_url
+      login_url = p.search(ret).group(1)
+      r = requests.request('GET',login_url)
+      r.raise_for_status()
+      print("success")
+   except:
+      print('failure')
+   
