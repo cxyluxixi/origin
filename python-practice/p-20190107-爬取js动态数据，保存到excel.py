76099e04c964f623964162f11277a2640f8c3148
cxyluxixi <632137620@qq.com>
2019-01-11 21:49:02 +0800

    删除多余重复文件

--- python-practice/p-20190107-爬取js动态数据，保存到excel.py
+++ python-practice/p-20190107-爬取js动态数据，保存到excel.py
@@ -79,3 +79,61 @@ for n in range(1, 10):
     time.sleep(random.randint(1,4))
 print('>Finished!')
 
+
+# 将爬到的数据，根据格式处理，
+
+# 切分区间数据
+def range2two(s, unit):
+    s = s.replace(unit, '')  #去掉单位k、年、人
+    s = s.replace(' ', '')  #去掉空格
+    l = s.split('-')
+    res = {
+        'low': str(l[0]), 
+        'high': str(l[1]) if len(l) > 1 else 'None'
+    }
+    return res
+
+
+
+labels=['positionId','positionName','workYear','education','createTime','salary','city','companyFullName','companySize','financeStage','firstType','secondType','details']
+labels2=['salary_low','salary_high','workYear_low','workYear_high','companySize_low','companySize_high']
+
+
+# 将json数据提取为列表line=[]
+def readJob(fileName):
+    with open(fileName,'r') as f:
+        job=json.load(f)
+        line=[]
+        for key in labels:
+            line.append(str(job[key]).replace(',','，')) 
+            #添加所有labels的字段，用中文逗号替换英文逗号，避免分割混乱
+            
+        salaries = range2two(job['salary'], 'k')
+        workYears = range2two(job['workYear'], '年')
+        companySizes = range2two(job['companySize'], '人')
+        line += [salaries['low'], salaries['high']]
+        line += [workYears['low'], workYears['high']]
+        line += [companySizes['low'], companySizes['high']]
+
+        return ','.join(line)
+        
+# 表头 
+labels=['positionId','positionName','workYear','education','createTime','salary','city','companyFullName','companySize','financeStage','firstType','secondType','details']
+labels2=['salary_low','salary_high','workYear_low','workYear_high','companySize_low','companySize_high']
+
+
+
+import os
+files = os.listdir('./data/lagou_ai/jobs/')  #获取所有文件列表
+with open('./data/lagou_ai/jobs.csv', 'w', encoding="gb18030") as f:
+    text = ''
+    text += ','.join(labels + labels2)
+
+    for fname in files:
+        if not fname.find('.json')==-1: 
+            text += '\n'
+            text += readJob('./data/lagou_ai/jobs/' + fname)
+    f.write(text)
+    f.close()
+    print('>>OK!')
+
