92c9e8f76982be695dac22a2e71aea088b9ea3c1
cxyluxixi <42094356+cxyluxixi@users.noreply.github.com>
2019-01-17 14:40:01 +0800

    增加注释

--- python-practice/book/book/spiders/bookspider.py
+++ python-practice/book/book/spiders/bookspider.py
@@ -2,6 +2,16 @@
 import scrapy
 from book.items import BookItem
 
+# 创建project  scrapy startproject spidername
+# cd切换目录到爬虫目录，编写items.py， 设置数据表中需要有哪些指标，如：name=spider.Field()
+# 开始编写spider文件，parse()写解析式，提取上一步需要的那些items，相当于requests中bs4的find，find_all表达式
+# 写中间件，爬虫伪装，设置代理ip，my_proxy 服务器等
+# 写pipielines.py,一般是关于数据储存，判断清洗等爬下来之后的处理。
+# 设置settings.py， 一般是关于连接服务器和爬虫伪装的一些设置：开启管道,  user_agent,  headers,
+#DOWNLOADER_MIDDLEWARES = {
+#    'book.middlewares.BookDownloaderMiddleware': 543,
+#    'book.middlewares.my_proxy':数字越小，优先级越高,
+#}
 
 class BookspiderSpider(scrapy.Spider):
     name = 'bookspider'
